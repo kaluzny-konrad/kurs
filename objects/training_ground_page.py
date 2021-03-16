47bb5b47e00b7f011cdff93400b585e12bd40896
konrad-kaluzny-ceneo <72854104+konrad-kaluzny-ceneo@users.noreply.github.com>
2021-03-16 06:58:46 +0100

    commit 5

--- objects/training_ground_page.py
+++ objects/training_ground_page.py
@@ -1,17 +1,25 @@
-class BaseElement():
-    def __init__(self):
-        pass
-
-class Inputs(BaseElement):
-    def __init__(self):
-        pass
-
 class TrainingGroundPage():
     def __init__(self, browser):
         self.browser = browser
-        url = 'https://techstepacademy.com/training-ground'
-        inputs_names = {'ipt1', 'ipt2'}
-        self.inputs = Inputs(inputs_names)
+        self.url = 'https://techstepacademy.com/training-ground'
 
     def go(self):
+        self.browser.get(self.url)
+        return None
+
+    def set_input(self, text_input):
+        my_input = self.browser.find_element_by_id("ipt1")
+        my_input.clear()
+        my_input.send_keys(text_input)
+        return None
+        
+    def get_input(self):
+        my_input = self.browser.find_element_by_id("ipt1")
+        text_input = my_input.get_attribute('value')
+        return text_input
+
+    def click_button(self):
         pass
+
+    def quit(self):
+        self.browser.quit()
